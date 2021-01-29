fa66a37816905de4f082038d30bc5a8fdbbf757a
Kuddleman <edl1155@gmail.com>
2021-01-29 14:36:59 -0800

    complete task 1208

---
+++ src/com/codegym/task/task12/task1208/Solution.java
@@ -0,0 +1,36 @@
+package com.codegym.task.task12.task1208;
+
+/* 
+Freedom of the press
+
+*/
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static void print(String s) {
+
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(HashMap<String, Integer> map) {
+
+    }
+
+    public static void print( int[] myInts) {
+
+    }
+
+    public static void print (ArrayList<String> languages) {
+
+    }
+}
