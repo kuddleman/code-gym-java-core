2bce2715f4b1ce2bf7512a42e9f8d43075264fea
Kuddleman <edl1155@gmail.com>
2021-01-29 14:33:05 -0800

    complete task 1207

---
+++ src/com/codegym/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task12.task1207;
+
+/* 
+int and Integer
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+      Solution.print(new Integer(2));
+      Solution.print(2);
+    }
+
+    //write your code here
+    public static void print(int i) {
+        System.out.println(i);
+    }
+
+    public static void print(Integer i) {
+        System.out.println(i);
+    }
+}
