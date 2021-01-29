9a1b42c22eb20460eaeaf58cee6c4274a6bf9304
Kuddleman <edl1155@gmail.com>
2021-01-29 09:52:43 -0800

    initial commit

---
+++ src/com/codegym/task/task11/task1123/Solution.java
@@ -0,0 +1,44 @@
+package com.codegym.task.task11.task1123;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndMaximum(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndMaximum(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        //write your code here
+        int minimum = array[0];
+        int maximum = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minimum) {
+                minimum = array[i];
+            }
+            if (array[i] > maximum) {
+                maximum = array[i];
+            }
+        }
+
+        return new Pair<Integer, Integer>(minimum, maximum );
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ src/com/codegym/task/task12/task1206/Solution.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task12.task1206;
+
+/* 
+We're on overload!
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+sssssssssssss
+    //write your code here
+    //This is a test
+    //This is a test
+}
