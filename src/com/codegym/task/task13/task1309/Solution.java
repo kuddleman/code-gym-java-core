2a0126a97b4c4e627c10654fbe6ee0746520fd84
Kuddleman <edl1155@gmail.com>
2021-01-31 14:27:12 -0800

    complete task 1309

---
+++ src/com/codegym/task/task13/task1309/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task13.task1309;
+
+/* 
+All that moves
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    interface CanMove {
+        Double speed();
+    }
+
+    interface CanFly extends CanMove {
+        Double speed(CanFly obj);
+
+
+    }
+
+
+}
