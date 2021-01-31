823b7dcbec7c3b34d7ffa756c6743f35d9e0109a
Kuddleman <edl1155@gmail.com>
2021-01-30 16:45:34 -0800

    complete task 1220

---
+++ src/com/codegym/task/task12/task1220/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task12.task1220;
+
+/* 
+Human class and CanRun and CanSwim interfaces
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // Add public interfaces and a public class here
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+
+    }
+
+    public abstract class Human implements CanRun, CanSwim {
+
+    }
+
+
+}
