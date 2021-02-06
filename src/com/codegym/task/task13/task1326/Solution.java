058a57e8d4d242ff50af58462d1f6e10b7a3dafe
Kuddleman <edl1155@gmail.com>
2021-02-05 16:55:04 -0800

    complete task 1327

---
+++ src/com/codegym/task/task13/task1326/Solution.java
@@ -0,0 +1,42 @@
+package com.codegym.task.task13.task1326;
+
+/* 
+Sorting even numbers from a file
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sourceFileName = reader.readLine();
+        java.io.FileInputStream fileInputStream = new java.io.FileInputStream(sourceFileName);
+
+        Scanner scanner =  new Scanner(fileInputStream);
+        List<Integer> data = new ArrayList<Integer>();
+
+        while (scanner.hasNext()) {
+            int value = scanner.nextInt();
+            if (value % 2 == 0) data.add(value);
+        }
+
+        Collections.sort(data);
+
+        for (Integer value : data) {
+            System.out.println(value);
+        }
+
+        scanner.close();
+        fileInputStream.close();
+
+
+    }
+}
