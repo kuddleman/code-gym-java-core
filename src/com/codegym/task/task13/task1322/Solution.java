58455f89daeeca01ca2a0ebf1be2c928c3d1137b
Kuddleman <edl1155@gmail.com>
2021-02-05 16:21:15 -0800

    complete task 1322

---
+++ src/com/codegym/task/task13/task1322/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task13.task1322;
+
+/* 
+SimpleObject interface
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        SimpleObject<String> stringObject = new StringObject();
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+
+    static class StringObject implements SimpleObject<String> {
+        public SimpleObject<String> getInstance() {
+            return null;
+        }
+    }
+}
