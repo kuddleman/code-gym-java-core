5e4701ce486d49c79455c5a6221ef5551b906e60
Kuddleman <edl1155@gmail.com>
2021-02-05 16:23:40 -0800

    complete task 1323

---
+++ src/com/codegym/task/task13/task1323/Solution.java
@@ -0,0 +1,29 @@
+package com.codegym.task.task13.task1323;
+
+/* 
+Updatable interface in the Screen class
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable extends Selectable {
+        void refresh();
+    }
+
+    class Screen implements Updatable{
+        public void refresh() {
+
+        }
+        public void onSelect() {
+
+        }
+
+    }
+}
